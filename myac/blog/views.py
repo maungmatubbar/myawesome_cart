e8e2045c887db167b5b15f72ff12fd72001f23e0
maungmatubbar <maungmatubbar@gmail.com>
2025-01-20 00:08:57 +0600

    Update Blogs And show content

--- myac/blog/views.py
+++ myac/blog/views.py
@@ -1,9 +1,14 @@
 from django.http import HttpResponse
 from django.shortcuts import render
-
+from .models import Blog
 # Create your views here.
 def index(request):
+    
     return render(request, 'blog/index.html')
 
-def blogpost(request):
-    return render(request, 'blog/blogpost.html')
+def blogpost(request, id):
+    post = Blog.objects.filter(post_id = id)[0]
+    print(post)
+
+
+    return render(request, 'blog/blogpost.html',{'post':post})
